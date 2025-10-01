548973b9f19ae2705d46d27576a6994df7807e66
regidentkalbar-jak <regidentkalbar@gmail.com>
2025-10-01 11:10:56 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,29 @@
-const CACHE_NAME = 'keuangan-cache-v1';
-const urlsToCache = [
+// minimal service worker for caching basic app shell
+const CACHE_NAME = 'catatan-keuangan-shell-v1';
+const URLS = [
   '/',
   '/index.html',
   '/style.css',
   '/script.js',
-  '/manifest.json'
+  '/manifest.json',
+  // tambahkan icon jika ada
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
 ];
 
-// Install service worker
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS).catch(() => {}))
   );
+  self.skipWaiting();
 });
 
-// Fetch from cache
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(resp => resp || fetch(event.request).catch(() => resp))
   );
 });
 
-// Update cache when new version found
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.filter(name => name !== CACHE_NAME)
-                  .map(name => caches.delete(name))
-      );
-    })
-  );
+  event.waitUntil(clients.claim());
 });
